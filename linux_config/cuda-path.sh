ad3899bcbfebbb136d9363a0ecf90694f50af72b
William Sadler <wmsadler@gmail.com>
2018-10-08 10:31:18 -0500

    added linux profile config files

---
+++ linux_config/cuda-path.sh
@@ -0,0 +1,4 @@
+# shellcheck shell=sh
+
+# Expand $PATH to include the directory where snappy applications go.
+export PATH=/usr/local/cuda/bin:$PATH
---
+++ linux_config/opencv-path.sh
@@ -0,0 +1,4 @@
+# shellcheck shell=sh
+
+# Expand $PATH to include the directory where snappy applications go.
+export OPENCV_TEST_DATA_PATH=/home/wsadler/src/opencv_extra/testdata/
