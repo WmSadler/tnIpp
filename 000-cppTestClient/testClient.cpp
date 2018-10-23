eb3e55b7b00079394829ab153e42cfc8e18dd92f
William Sadler <wmsadler@gmail.com>
2018-10-23 11:52:09 -0500

    fixed color problem - staged ipp connection code

--- 000-cppTestClient/testClient.cpp
+++ 000-cppTestClient/testClient.cpp
@@ -8,96 +8,110 @@
 #include <syslog.h>
 #include <zmq.h>
 
+#include "../include/zhelpers.hpp"
 #include "../include/INIReader.h"
 
+using namespace zmq;
 using namespace cv;
-using std::string;
-using std::cout;using std::cerr;using std::cerr; using std::endl;
-using std::exception;
+using namespace std;
 
 int main (int argc, char **argv)
 {
-    int returnStatus = 0;
-    int rc = 0;
+	int returnStatus = 0;
+	int rc = 0;
+	int keyPress;
+	bool done = false;
+	bool sendToFr;
 
-    string ippBrokerIp, ippBrokerPort, ippBrokerUri;
-    int capW, capH;
+	Mat imgRawCap;
+	Mat imgPostProc;
+	std::vector<uchar> imgJpeg;
+	int capW, capH;
 
-    setlogmask(LOG_UPTO (LOG_INFO));
-    openlog("tnIppTestClient",LOG_PERROR | LOG_CONS | LOG_PID, LOG_DAEMON);
+	float fps = 0.0;
+	string displayFps;
+	char *fpsNum;
+	int frameCounter=0;
 
-    string iniFileName="default.ini";
+	string ippBrokerIp, ippBrokerPort, ippBrokerUri;
 
-    if (argc == 2) {
-        iniFileName = argv[1];
-    }
-    syslog(LOG_INFO,"Test Client start with %s", iniFileName.c_str());
+	setlogmask(LOG_UPTO (LOG_INFO));
+	openlog("tnIppTestClient",LOG_PERROR | LOG_CONS | LOG_PID, LOG_DAEMON);
 
-    try {
-        INIReader iniBuffer("../iniFiles/"+iniFileName);
-        if (!iniBuffer.ParseError()) {
-            ippBrokerIp = iniBuffer.Get("ippBroker","ippBrokerIp","");
-            ippBrokerPort = iniBuffer.Get("ippBroker","ippBrokerPort","");
-            ippBrokerUri = "tcp://"+ippBrokerIp+":"+ippBrokerPort;
-            capW = iniBuffer.GetInteger("ippClient","camWidth",1280);
-            capH = iniBuffer.GetInteger("ippClient","camHeight",720);
-            syslog(LOG_INFO,"Conncection attempt to ippBroker at %s",ippBrokerUri.c_str());
-            syslog(LOG_INFO,"Using %i,%i camera size",capW,capH);
+	string iniFileName="default.ini";
 
-            // starting capture from camera
-            VideoCapture cam(0);
-            cam.set(CV_CAP_PROP_FRAME_WIDTH,capW);
-            cam.set(CV_CAP_PROP_FRAME_HEIGHT,capH);
+	if (argc == 2) {
+		iniFileName = argv[1];
+	}
+	syslog(LOG_INFO,"Test Client start with %s", iniFileName.c_str());
 
-            namedWindow("VideoImage", WINDOW_AUTOSIZE);
-            namedWindow("Snapped", WINDOW_AUTOSIZE);
-            namedWindow("Processed", WINDOW_AUTOSIZE);
+	try {
+		INIReader iniBuffer("../iniFiles/"+iniFileName);
+		if (!iniBuffer.ParseError()) {
+			ippBrokerIp = iniBuffer.Get("ippBroker","ippBrokerIp","");
+			ippBrokerPort = iniBuffer.Get("ippBroker","ippBrokerPort","");
+			ippBrokerUri = "tcp://"+ippBrokerIp+":"+ippBrokerPort;
+			sendToFr = iniBuffer.GetBoolean("ippClient","sendToFr",false);
+			capW = iniBuffer.GetInteger("ippClient","camWidth",1280);
+			capH = iniBuffer.GetInteger("ippClient","camHeight",720);
+			syslog(LOG_INFO,"Conncection attempt to ippBroker at %s",ippBrokerUri.c_str());
+			syslog(LOG_INFO,"Using %i,%i camera size",capW,capH);
 
-            int keyPress;
-            bool done = false;
-            Mat imgRawCap(capH,capW,CV_8UC4);
-            Mat imgPostProc(capH,capW,CV_8UC4);
-            std::vector<uchar> imgJpeg;
-            float fps = 0.0;
-            int frameCounter=0;
+			// connect to IPP zmq endpoint
+			context_t context();
 
-            std::time_t tStart = std::time(0);
-            while (!done) {
-                cam >> imgRawCap;
-                //cout << rawImgBGR.depth() << endl;
-                frameCounter++;
+			// starting capture from camera
+			VideoCapture cam(0);
+			cam.set(CV_CAP_PROP_FRAME_WIDTH,capW);
+			cam.set(CV_CAP_PROP_FRAME_HEIGHT,capH);
 
-                // send to IPP Here
-                {
-                    //cvtColor(rawImgBGR,rawImgRGB, COLOR_BGR2RGB);
-                    imwrite("test1.jpg",imgRawCap);
-                    imencode(".jpeg",imgRawCap,imgJpeg);
-                    imdecode(imgJpeg,0,&imgPostProc);
-                    imwrite("test2.jpg",imgPostProc);
-                }
+			namedWindow("VideoImage", WINDOW_AUTOSIZE|WINDOW_GUI_EXPANDED);
+			namedWindow("Processed", WINDOW_AUTOSIZE);
 
-                std::time_t tNow = std::time(0);
-                fps = frameCounter / difftime(tNow,tStart);
-                putText(imgRawCap, cv::format("Avg FPS=%3.1f",fps),
-                        cv::Point(30,30),
-                        cv::FONT_HERSHEY_SIMPLEX,1.0,
-                        cv::Scalar(0,0,0));
+			std::time_t tStart = std::time(0);
+			while (!done) {
+				cam >> imgRawCap;
+				//cout << rawImgBGR.depth() << endl;
+				frameCounter++;
 
-                imshow("VideoImage", imgRawCap);
-                imshow("Snapped", imgJpeg);
-                imshow("Processed", imgPostProc);
-                keyPress = waitKey(10);
-                done = (keyPress==27);
-            }
-        } else {
-            syslog(LOG_ERR, "Ini File Exception");
-            returnStatus = -1;
-        }
-    } catch (...){
-        syslog(LOG_ERR, "terminating with exception");
-        returnStatus = -1;
-    }
+				if (sendToFr){
+					imencode(".jpeg",imgRawCap,imgJpeg);
+					// send here
+					imgPostProc = imdecode(imgJpeg,IMREAD_ANYCOLOR|IMREAD_ANYDEPTH);
+				} else{
+					imgPostProc = imgRawCap;
+				}
 
-    closelog();
-    return(returnStatus);
+				std::time_t tNow = std::time(0);
+				fps = frameCounter / difftime(tNow,tStart);
+				asprintf(&fpsNum,"%.1f",fps);
+				if (sendToFr) {
+					displayFps = "SENDING FPS:" + string(fpsNum);
+				} else {
+					displayFps = "LOCAL FPS:" + string(fpsNum);
+				}
+				free(fpsNum);
+				displayStatusBar("VideoImage",displayFps,0);
+				imshow("VideoImage", imgRawCap);
+				imshow("Processed", imgPostProc);
+
+				keyPress = waitKey(1);
+				switch (keyPress) {
+					case 's' : sendToFr = !sendToFr;break;
+					case 'q' :
+					case 27 : done = true;break;
+				}
+			}
+		} else {
+			syslog(LOG_ERR, "Ini File Exception");
+			returnStatus = -1;
+		}
+	} catch (...){
+		syslog(LOG_ERR, "terminating with exception");
+		returnStatus = -1;
+	}
+
+	destroyAllWindows();
+	closelog();
+	return(returnStatus);
 }
